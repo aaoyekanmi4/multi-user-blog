7408ad18c24f8e6e495165cc8bc596f41425866f
aaoyekanmi4 <aaoyekanmi4@gmail.com>
2016-09-20 17:49:47 -0500

    Changed e_comment to comm

--- handlers/delete_comment.py
+++ handlers/delete_comment.py
@@ -10,11 +10,11 @@ class DeleteComment(Handler):
     def get(self, post_id, c_id):
 
         self.p = BlogInfo.get_by_id(int(post_id))
-        self.e_comment = Comment.get_by_id(int(c_id), parent=self.p)
+        self.comm = Comment.get_by_id(int(c_id), parent=self.p)
         self.this_user = self.get_user()
         if self.this_user:
-            if self.e_comment and self.e_comment.comment_author == self.this_user:
-                self.e_comment.delete()
+            if self.comm and self.comm.comment_author == self.this_user:
+                self.comm.delete()
                 self.redirect("/blog/%s" % post_id)
             else:
                 self.redirect('/blog')
--- handlers/edit_comment.py
+++ handlers/edit_comment.py
@@ -11,15 +11,15 @@ class EditComment(Handler):
     def get(self, post_id, c_id):
 
         self.p = BlogInfo.get_by_id(int(post_id))
-        self.e_comment = Comment.get_by_id(int(c_id), parent=self.p)
+        self.comm = Comment.get_by_id(int(c_id), parent=self.p)
         self.post_comments = Comment.all().ancestor(self.p).fetch(limit=10)
         self.this_user = self.get_user()
 
-        params = dict (comment_to_edit = self.e_comment,
+        params = dict (comment_to_edit = self.comm,
             this_user = self.this_user, post_comments = self.post_comments)
 
         if self.this_user:
-            if self.e_comment and self.e_comment.comment_author == self.this_user:
+            if self.comm and self.comm.comment_author == self.this_user:
                 params.update(main.loggedin_params)
                 self.render("editcomment.html", **params)
             else:
@@ -32,17 +32,17 @@ class EditComment(Handler):
 
         self.comment_text = self.request.get("comment")
         self.p = BlogInfo.get_by_id(int(post_id))
-        self.e_comment = Comment.get_by_id(int(c_id), parent=self.p)
+        self.comm = Comment.get_by_id(int(c_id), parent=self.p)
         self.post_comments = Comment.all().ancestor(self.p).fetch(limit=10)
         self.this_user = self.get_user()
 
-        params = dict(comment_to_edit = self.e_comment,
+        params = dict(comment_to_edit = self.comm,
             this_user = self.this_user, post_comments = self.post_comments)
 
         if not self.this_user:
             self.redirect('/login')
 
-        if self.this_user == self.e_comment.comment_author:
+        if self.this_user == self.comm.comment_author:
 
             if not self.comment_text:
                 params.update(main.loggedin_params)
@@ -50,11 +50,11 @@ class EditComment(Handler):
                 self.render("editcomment.html", **params)
 
             else:
-                self.e_comment = Comment.get_by_id(int(c_id), parent= self.p)
-                self.e_comment.comment_text = self.comment_text
-                self.e_comment.put()
+                self.comm = Comment.get_by_id(int(c_id), parent= self.p)
+                self.comm.comment_text = self.comment_text
+                self.comm.put()
 
-                self.redirect("/blog/%s" % self.e_comment.associated_post)
+                self.redirect("/blog/%s" % self.comm.associated_post)
         else:
             params.update(main.loggedin_params)
             params["error"] = "You may only edit your own comments."
